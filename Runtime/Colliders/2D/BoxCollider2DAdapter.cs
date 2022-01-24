ddf4593e8e0f18ec04000dde62701dd860046523
Hyago Oliveira <hyagogow@gmail.com>
2022-01-24 18:53:36 -0300

    Add BoxCollider2DAdapter

---
+++ Runtime/Colliders/2D/BoxCollider2DAdapter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ActionCode.ColliderAdapter
+{
+    /// <summary>
+    /// Adapter Component for 2D Box Collider.
+    /// </summary>
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(BoxCollider2D))]
+    [AddComponentMenu("Physics 2D/Box Collider 2D Adapter")]
+    public sealed class BoxCollider2DAdapter : Abstract2DColliderAdapter<BoxCollider2D>
+    {
+        public override Vector3 Size { set => collider.size = value; }
+
+        protected override bool InternalCast(Vector3 direction, float maxDistance, int layerMask,
+            out RaycastHit2D collisionHit, bool draw) =>
+            collider.Cast(DEFAULT_OFFSET, direction, maxDistance, layerMask, out collisionHit,
+                ForwardAngle, minDepth, maxDepth, DEFAULT_SKIN, draw);
+    }
+}
