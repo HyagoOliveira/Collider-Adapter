1cdf941736e54ce48a0fe72b73b30b66be4884aa
Hyago Oliveira <hyagogow@gmail.com>
2022-01-24 18:53:58 -0300

    Add CapsuleCollider2DAdapter

---
+++ Runtime/Colliders/2D/CapsuleCollider2DAdapter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ActionCode.ColliderAdapter
+{
+    /// <summary>
+    /// Adapter Component for 2D Capsule Collider.
+    /// </summary>
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(CapsuleCollider2D))]
+    [AddComponentMenu("Physics 2D/Capsule Collider 2D Adapter")]
+    public sealed class CapsuleCollider2DAdapter : Abstract2DColliderAdapter<CapsuleCollider2D>
+    {
+        public override Vector3 Size { set => collider.size = value; }
+
+        protected override bool InternalCast(Vector3 direction, float maxDistance, int layerMask,
+            out RaycastHit2D collisionHit, bool draw) =>
+            collider.Cast(DEFAULT_OFFSET, direction, maxDistance, layerMask, out collisionHit,
+                ForwardAngle, minDepth, maxDepth, DEFAULT_SKIN, draw);
+    }
+}
